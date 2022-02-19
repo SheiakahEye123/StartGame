53046f88dd637e46fcdcfb1e7c3e6ff5f881a9ae
BuildTools <unconfigured@null.spigotmc.org>
2022-02-18 22:12:45 -0800

    Initial commit

---
+++ src/Minekroft/Display.java
@@ -0,0 +1,99 @@
+package Minekroft;
+
+import Minekroft.Graphics.Render;
+import Minekroft.Graphics.Screen;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+public class Display extends Canvas implements Runnable {
+
+    public static final int width = 1980;
+    public static final int height = 1080;
+    public static final String title = "Minekroft...";
+
+    private Thread thread;
+    private Screen screen;
+    private BufferedImage img;
+    private boolean running = false;
+    private Render render;
+    private int[] pixels;
+
+    public Display() {
+        screen = new Screen(width, height);
+        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        pixels = ((DataBufferInt)img.getRaster().getDataBuffer()).getData();
+    }
+
+    private void start() {
+        if (running) return;
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+
+        System.out.println("Working!");
+    }
+
+    private void stop(){
+        if (!running) return;
+        running = false;
+        try {
+            thread.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    public void run() {
+        while (running) {
+            tick();
+            render();
+        }
+    }
+
+    private void tick() {
+
+    }
+
+    private void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+
+        screen.render();
+
+        for (int i = 0; i<width*height;i++) {
+            pixels[i] = screen.pixels[i];
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        g.drawImage(img, 0, 0, width, height,null);
+        g.dispose();
+        bs.show();
+
+
+    }
+
+    public static void main(String[] args) {
+        Display game = new Display();
+        JFrame frame = new JFrame();
+        frame.add(game);
+        frame.pack();
+        frame.setTitle(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(width,height);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        System.out.println("Starting...");
+        game.start();
+
+    }
+}
---
+++ src/Minekroft/Graphics/Render.java
@@ -0,0 +1,26 @@
+package Minekroft.Graphics;
+
+public class Render {
+    public final int width;
+    public final int height;
+    public final int[] pixels;
+
+    public Render(int width, int height) {
+        this.width = width;
+        this.height = height;
+        pixels = new int[width * height];
+    }
+
+    public void draw(Render render, int xOffset, int yOffset) {
+        for (int y = 0; y<render.height; y++ ) {
+            int yFix = y + yOffset;
+            for (int x = 0; x<render.height; x++ ) {
+                int xFix = x + yOffset;
+
+                pixels[xFix + yFix * width] = render.pixels[x + y * render.width];
+            }
+        }
+
+    }
+
+}
